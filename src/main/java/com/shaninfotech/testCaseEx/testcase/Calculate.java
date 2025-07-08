e43c6cbedb6c650d675c7d189bbf27f01ba5e572
Sharanya BK <sharanyabk2002@gmail.com>
2025-07-08 15:16:02 +0530

    JunitTestcases

---
+++ src/main/java/com/shaninfotech/testCaseEx/testcase/Calculate.java
@@ -0,0 +1,13 @@
+package com.shaninfotech.testCaseEx.testcase;
+
+public class Calculate {
+	public int add(int a, int b) {
+		return a+b;
+	
+}
+	
+	public int areaOfCircle(int r) {
+		return (int) (3.14*r*r);
+	}
+
+}
